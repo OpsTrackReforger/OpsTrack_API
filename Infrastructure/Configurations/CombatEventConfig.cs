64b15a04ec2802146ed11e2a1ac3335aff80e430
stoffer123 <christophermikkelsen@live.dk>
2025-09-26 22:33:58 +0200

    Added configuration class and migrations

---
+++ Infrastructure/Configurations/CombatEventConfig.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class CombatEventConfig : IEntityTypeConfiguration<CombatEvent>
+    {
+        public void Configure(EntityTypeBuilder<CombatEvent> builder)
+        {
+            //primary key also foreign key to Event
+            builder.HasKey(ce => ce.EventId);
+
+            builder.HasOne(c => c.Event)
+                .WithOne() //Event doesnt know CombatEvent
+                .HasForeignKey<CombatEvent>(ce => ce.EventId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //Relations to Actor (nullable)
+            builder.HasOne(ce => ce.Actor)
+                .WithMany() //Player doesnt know CombatEvents
+                .HasForeignKey(ce => ce.ActorId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            //Relations to Victim (nullable)
+            builder.HasOne(ce => ce.Victim)
+                .WithMany() //Player doesnt know CombatEvents
+                .HasForeignKey(ce => ce.VictimId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            //Fields
+            builder.Property(ce => ce.Weapon)
+                .HasMaxLength(100)
+                .IsRequired(false);
+
+            builder.Property(ce => ce.Distance)
+                .IsRequired();
+
+            builder.Property(ce => ce.IsTeamKill)
+                .IsRequired();
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20250926202743_InitCombatEvents.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitCombatEvents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CombatEvent",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ActorId = table.Column<string>(type: "TEXT", nullable: true),
+                    VictimId = table.Column<string>(type: "TEXT", nullable: true),
+                    Weapon = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    Distance = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsTeamKill = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CombatEvent", x => x.EventId);
+                    table.ForeignKey(
+                        name: "FK_CombatEvent_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "EventId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CombatEvent_Players_ActorId",
+                        column: x => x.ActorId,
+                        principalTable: "Players",
+                        principalColumn: "GameIdentity",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_CombatEvent_Players_VictimId",
+                        column: x => x.VictimId,
+                        principalTable: "Players",
+                        principalColumn: "GameIdentity",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CombatEvent_ActorId",
+                table: "CombatEvent",
+                column: "ActorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CombatEvent_VictimId",
+                table: "CombatEvent",
+                column: "VictimId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CombatEvent");
+        }
+    }
+}
